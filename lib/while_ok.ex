7d57f7565eea6715aae946c7985d648b27d1abd5
mishaconway <mishaaconway@gmail.com>
2021-07-21 22:25:34 -0700

    Adds each function

--- lib/while_ok.ex
+++ lib/while_ok.ex
@@ -1,18 +1,14 @@
 defmodule WhileOk do
-  @moduledoc """
-  Documentation for `WhileOk`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> WhileOk.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  # Enumerates over enumerable while the given func returns an ok tuple.
+  # If an error tuple is ever returned, then enumeration is halted and the error value is returned.
+  def each(enumerable, func, [return_when_all_ok: return_when_all_ok] = opts) do
+    case enumerable do
+      [] -> return_when_all_ok
+      [head | tail] ->
+        case func.(head) do
+          {:ok, _ } -> each(tail, func, opts)
+          {:error, _} = err -> err
+        end
+    end
   end
 end
