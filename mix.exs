68dd7146b5960cfba04a9cf3be33f2069d7f3321
mishaconway <mishaaconway@gmail.com>
2021-07-21 22:35:42 -0700

    updates

--- mix.exs
+++ mix.exs
@@ -6,6 +6,8 @@ defmodule WhileOk.MixProject do
       app: :while_ok,
       version: "0.1.0",
       elixir: "~> 1.12-rc",
+      description: description(),
+      package: package(),
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
@@ -23,6 +25,22 @@ defmodule WhileOk.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+    ]
+  end
+
+  defp description() do
+    "Adds utilities for enumerating over collections until there is an error"
+  end
+
+  defp package() do
+    [
+      # This option is only needed when you don't want to use the OTP application name
+      name: "while_ok",
+      # These are the default files included in the package
+      files: ~w(lib priv .formatter.exs mix.exs README* readme* LICENSE*
+                license* CHANGELOG* changelog* src),
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/MishaConway/while_ok"}
     ]
   end
 end
